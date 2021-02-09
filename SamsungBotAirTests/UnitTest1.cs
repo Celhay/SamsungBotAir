eadc13dbc40a06ee365a651d850b6cfc5b7bef85
Celhay <>
2021-02-09 10:09:39 +0100

    ajout tests

---
+++ SamsungBotAirTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SamsungBotAirTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
